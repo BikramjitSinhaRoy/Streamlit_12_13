c2503a8c5636c9a17da138811ada5456f86d60ad
BikramjitSinhaRoy <bikramjitsinharoy10@gmail.com>
2023-11-13 19:47:46 -0500

    day is over

--- penguins.py
+++ penguins.py
@@ -3,18 +3,15 @@ import pandas as pd
 import seaborn as sns
 import streamlit as st
 st.title("Palmer's Penguins")
-st.markdown("Use this Streamlit app to make your own scatterplot about
-penguins!")
+st.markdown("Use this Streamlit app to make your own scatterplot about penguins!")
 
 selected_x_var = st.selectbox(
     "What do you want the x variable to be?",
-    ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_
-mass_g"], )
+    ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"], )
 
 selected_y_var = st.selectbox(
     "What about the y?",
-    ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_
-mass_g"],
+    ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"],
 )
 
 penguins_df = pd.read_csv("penguins.csv")
@@ -30,3 +27,12 @@ alt_chart = (
     .interactive()
 )
 st.altair_chart(alt_chart, use_container_width=True)
+
+
+
+
+
+
+
+
+
